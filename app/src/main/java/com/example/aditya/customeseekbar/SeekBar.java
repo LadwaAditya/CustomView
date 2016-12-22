5083431bb68c41a285bef25b201ef53e1968a217
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-12-22 20:04:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aditya/customeseekbar/SeekBar.java
@@ -0,0 +1,21 @@
+package com.example.aditya.customeseekbar;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Custome SeekBar
+ * Created by Aditya on 22-Dec-16.
+ */
+
+public class SeekBar extends View {
+    public SeekBar(Context context) {
+        super(context);
+    }
+
+    public SeekBar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
